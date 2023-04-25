8487683ff29eafd40372687c5f95bd832590a7ae
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-04-25 10:53:23 +0500

    minor changes

--- src/app/api/orders/[slug]/route.ts
+++ src/app/api/orders/[slug]/route.ts
@@ -1,24 +1,94 @@
 import connectDb from "@/lib/db";
-import { authUser } from "@/utils/auth";
-import { NextRequest, NextResponse } from "next/server";
+import { IGetUserAuthInfoRequest, authUser } from "@/utils/auth";
+import { NextResponse } from "next/server";
 
 export const GET = async (
-  req: NextRequest,
+  req: IGetUserAuthInfoRequest,
   { params }: { params: { slug: string } }
 ) => {
   try {
     const conn = connectDb();
-    console.log("APIAFJSD");
-
     await authUser(req);
-    // const books = await conn`SELECT * FROM books
-    //   WHERE id = ${params.slug}`;
-    // if (books.length > 0)
-    //   return new NextResponse(JSON.stringify({ book: books[0] }));
-    // return new NextResponse(
-    //   JSON.stringify({ status: 200, message: "Invalid Book Id.!" })
-    // );
+    if (req.user) {
+      const orders = await conn`SELECT id, customerName,bookIsbn FROM orders
+        WHERE customerId = ${req.user} AND id = ${params.slug}`;
+      if (orders.length > 0)
+        return new NextResponse(JSON.stringify({ order: orders[0] }));
+    }
+    return new NextResponse(
+      JSON.stringify({ status: 200, message: "Invalid Order Id.!" })
+    );
   } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ status: 500, message: "Internal Server Error." })
+    );
+  }
+};
+export const DELETE = async (
+  req: IGetUserAuthInfoRequest,
+  { params }: { params: { slug: string } }
+) => {
+  try {
+    const conn = connectDb();
+    await authUser(req);
+    if (req.user) {
+      const order = await conn`SELECT id, customerName,bookIsbn FROM orders
+    WHERE customerId = ${req.user} AND id = ${params.slug}`;
+      if (order.length > 0) {
+        const orders = await conn`DELETE FROM orders
+          WHERE customerId = ${req.user} AND id = ${params.slug}`;
+      } else {
+        return new NextResponse(
+          JSON.stringify({ status: 200, message: "Invalid Order Id.!" })
+        );
+      }
+    }
+    return new NextResponse(
+      JSON.stringify({ status: 200, message: "Successfully deleted an order." })
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ status: 500, message: "Internal Server Error." })
+    );
+  }
+};
+export const PATCH = async (
+  req: IGetUserAuthInfoRequest,
+  { params }: { params: { slug: string } }
+) => {
+  try {
+    const conn = connectDb();
+    await authUser(req);
+    const body = await req.json();
+
+    if (req.user) {
+      const order = await conn`SELECT id, customerName,bookIsbn FROM orders
+    WHERE customerId = ${req.user} AND id = ${params.slug}`;
+      if (order.length > 0) {
+        if (body.customerName) {
+          const orders = await conn`UPDATE orders
+          SET customerName = ${body.customerName}
+            WHERE customerId = ${req.user} AND id = ${params.slug}
+            RETURNING id, customerName, bookIsbn`;
+        } else {
+          return new NextResponse(
+            JSON.stringify({ message: "Enter 'customerName' to update." })
+          );
+        }
+      } else {
+        return new NextResponse(
+          JSON.stringify({ status: 200, message: "Invalid Order Id.!" })
+        );
+      }
+    }
+    return new NextResponse(
+      JSON.stringify({
+        status: 200,
+        message: "Successfully updated an order.",
+      })
+    );
+  } catch (error) {
+    console.log("🚀 ~ file: route.ts:87 ~ error:", error);
     return new NextResponse(
       JSON.stringify({ status: 500, message: "Internal Server Error." })
     );
